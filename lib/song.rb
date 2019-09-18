08dc6d3f295df2d6f5ffd4117ec9fd5172d73f5f
Ali Karimi <ajkarimi7@gmail.com>
2019-09-18 19:28:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,9 +39,16 @@ class Song
     @@genres.uniq
   end 
   
-  
-  # song.count 
-  
-  # song.artist_count
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count += 1
+      else
+        genre_count[genre] = 1 
+      end 
+    end 
+    genre_count
+  end 
   
 end 
